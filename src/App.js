1899ebca747972cb5685bf02515b681225af7608
Katherine de Vay <katdevay2@gmail.com>
2023-03-01 16:19:26 -0600

    Create react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="top"></div>
+      <div id="mid"></div>
+      <div id="foot"></div>
+    </div>
+  );
+}
+
+export default App;
