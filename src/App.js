5b9d7f7803b91b5362b4df9d0d0b5c3f4b79c533
Katherine de Vay <katdevay2@gmail.com>
2023-03-02 15:53:09 -0600

    Add component styling for dark/lightmode toggle

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import './App.css';
+import React, { useState, useEffect } from "react";
+import {ThemeProvider} from "styled-components";
+import { GlobalStyles } from "./components/Globalstyle";
+import { lightTheme, darkTheme } from "./components/Themes"
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Mid from './components/Mid';
 
 
-function App() {
+export default function App() {
+  const [mode, setMode] = useState('dark');
+
+  const toggleMode = () => {
+    mode === 'light' ? setMode('dark') : setMode('light')
+  }
+
   return (
-    <div className="App">
-      <Header></Header>
-      <Mid></Mid>
-      <Footer></Footer>
-    </div>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <>
+      <GlobalStyles/>
+      <div className="App">
+        <div></div>
+        <Header mode={mode}></Header>
+        <Mid></Mid>
+        <Footer></Footer>
+      </div>
+      </>
+    </ThemeProvider>
   );
 }
-
-export default App;
