034bcd40dd62b3c95e0c1953a92180a466be3e40
Katherine de Vay <katdevay2@gmail.com>
2023-03-10 10:21:23 -0600

    Fix harvard/odin buttons in resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -49,24 +49,21 @@ export default function Resume(props) {
                         <h5 className='resBody'>JUN 2018&nbsp;&nbsp; ⇛ &nbsp;&nbsp;MAY 2020</h5>
                         <ul>
                             <li className='resBody'>Developed and tested recipes.</li>
-                            <li className='resBody'>Contributed to daily round-table discussions to improve food quality and efficiency</li>
+                            <li className='resBody'>Contributed to daily round-table discussions to improve food quality/consistency and efficiency</li>
                         </ul>
             </div>
             <div>
                 <h2 className='resSubDiv'>COURSES</h2>
                     <div className="courseRow">
-                        <button href="https://pll.harvard.edu/course/cs50-introduction-computer-science?delta=0" className={buttonClass}>
-                            {/* <span> */}
-                                <img className="courseIcon" src={mode === 'dark' ? HarvardD : Harvard} alt="Logo for Harvard University"></img>
-                                <h3 className='resBody'>Harvard CS50x</h3>
-                            {/* </span> */}
-                        </button>
-                        <button className={buttonClass} href="https://www.theodinproject.com/about">
-                            <span>
-                                <img className="courseIcon" src={mode === 'dark' ? OdinD : Odin} alt="Logo for The Odin Project"></img>
-                                <h3 className='resBody'>The Odin Project</h3>
-                            </span>
-                        </button>
+                        <a className={buttonClass} target="_blank" href='https://pll.harvard.edu/course/cs50-introduction-computer-science?delta=0'>
+                            <img className="courseIcon" src={mode === 'dark' ? HarvardD : Harvard} alt="Logo for Harvard University"></img>
+                            <h3 className='resBody'>Harvard CS50x</h3>
+                        </a>
+                    
+                        <a className={buttonClass} target="_blank" className={buttonClass} href="https://www.theodinproject.com/about">
+                            <img className="courseIcon" src={mode === 'dark' ? OdinD : Odin} alt="Logo for The Odin Project"></img>
+                            <h3 className='resBody'>The Odin Project</h3>
+                        </a>                            
                     </div>
             </div>
             <div>
