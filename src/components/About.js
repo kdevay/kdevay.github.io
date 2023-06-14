dbc1f0b5e17c67bab97cece8157628491f22b175
Katherine de Vay <katdevay2@gmail.com>
2023-06-13 20:35:36 -0500

    Revise about me content

--- src/components/About.js
+++ src/components/About.js
@@ -10,14 +10,14 @@ export default function About(props) {
         <div id='aboutDiv'>
             <div id='aboutText'>
                 <p className='aboutHead'>Hi, I'm Kat!</p><br></br>
-                <p className='aboutBody'>I'm a developer from New Orleans, looking to join a team of passionate professionals.</p><br></br>
+                <p className='aboutBody'>I'm a full stack developer, looking to join a team of passionate professionals.</p><br></br>
                 <p className='aboutBody'>Let's work together!</p><br></br>
                 {
                     mode === 'dark' ? <DarkIcons></DarkIcons> : <LightIcons></LightIcons>
                 }
             </div>
             <div className={'frame' + mode}>
-                <img className='profile' src={prof}></img>
+                <img className='profile' src={prof} alt='profile of developer'></img>
             </div>
 
         </div>
