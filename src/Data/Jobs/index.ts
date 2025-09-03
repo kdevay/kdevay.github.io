8bda7eba788f1b316084386bd2bcb7c63511162d
kdevay <kat@wherewego.org>
2025-09-03 18:03:45 -0500

    refactor to isolate job data

---
+++ src/Data/Jobs/index.ts
@@ -0,0 +1,23 @@
+import budgetSimpleAccomplishments from './budgetSimpleAccomplishments';
+import whereWeGoAccomplishments from './whereWeGoAccomplishments';
+
+const jobs = [
+  {
+    companyName: 'WhereWeGo',
+    jobTitle: 'Software Engineer',
+    location: 'New Orleans, LA',
+    startDate: 'JUL 2023',
+    endDate: 'PRESENT',
+    accomplishments: whereWeGoAccomplishments,
+  },
+  {
+    companyName: 'BudgetSimple',
+    jobTitle: 'Contract Web Developer',
+    location: 'New Orleans, LA',
+    startDate: 'APR 2023',
+    endDate: 'JUL 2023',
+    accomplishments: budgetSimpleAccomplishments,
+  },
+];
+
+export default jobs;
