404482c031ef7ebc11f13cda4e534641a441fe3a
kdevay <kat@wherewego.org>
2025-09-03 19:37:16 -0500

    update theme for help my eyeballs

--- src/styles/Themes.js
+++ src/styles/Themes.js
@@ -1,14 +1,14 @@
 export const lightTheme = {
-    body: '#FFF',
-    text: '#000',
-    toggleBorder: '#FFF',
-    background: '#363537',
-    border: '4px solid #363537;'
-}
+  body: '#FFF',
+  text: '#000',
+  toggleBorder: '#FFF',
+  background: '#FFF',
+  border: '4px solid #363537;',
+};
 export const darkTheme = {
-    body: '#363537',
-    text: '#FAFAFA',
-    toggleBorder: '#6B8096',
-    background: '#000',
-    border: '4px solid #727d8a;'
-}
+  body: '#363537',
+  text: '#d8d8d8',
+  toggleBorder: '#6B8096',
+  background: '#191919',
+  border: '4px solid #727d8a;',
+};
